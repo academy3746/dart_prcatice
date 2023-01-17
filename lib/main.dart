686cf3824e7f2cb65c5c26042d14b440c8b4b8dd
academy3746 <academy3746@gmail.com>
2023-01-17 23:39:28 +0900

    2. Dart 언어 Basic Course

--- lib/main.dart
+++ lib/main.dart
@@ -47,5 +47,37 @@ void main() {
   }
   // ※ 모든 변수들은 기본적으로 non-nullable 타입이다!
   damn?.isNotEmpty;
-  // 변수 damn이 null이 아니라면? 비어있지 않는 속성을 반환할 것...
+  // -> 변수 damn이 null이 아니라면? 비어있지 않는 속성을 반환할 것...
+
+
+  // What is Final? Countdown? LoL...
+  // final fuck = "NickelBack";
+  const fuck = "NickelBack";
+  // fuck = "GreenDay";
+  /// -> The final variable 'fuck' can only be set once.
+  /// final 타입으로 지정된 변수는 수정 불가...
+
+
+  // LATE VS CONSTANT
+  late final fantasy;
+  // 지금 당장은 데이터 값 없이 비어있는 변수타입인데...
+  fantasy = "1210038519af4g2910";
+  // API라든지... 나중에 넘어오는 값이 있으면 언제 어디서든 변수에 할당 가능!
+  // 수정되면 안되는데.. 난 이 값이 뭔지 아직 모른다...
+
+  if (fantasy != null) {
+    if (kDebugMode) {
+      print(fantasy);
+    }
+  }
+
+  const sex = "male";
+  // 난 이 값이 무엇인지 알고 있다!
+  // 그리고 수정될 수 없는 값이지...
+
+  if (sex.isNotEmpty) {
+    if (kDebugMode) {
+      print(sex);
+    }
+  }
 }
